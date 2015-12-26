2fb0460e00e17c9f975e3aee04655b741b40311f
niklas <noresjo@gmail.com>
2015-12-26 21:23:15 +0100

    Make gulp serve the default target

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,8 @@ var gulp = require('gulp');
 var electron = require('electron-connect').server.create();
 let jade = require('gulp-jade');
 
-
-
 gulp.task('transpile', function () {
-      gulp.src('./*.jade')
+    gulp.src('./*.jade')
         .pipe(jade({
             pretty: true,
             locals: {}
@@ -14,7 +12,7 @@ gulp.task('transpile', function () {
         .pipe(gulp.dest('./'))
 });
 
-gulp.task('serve', ['transpile'], function () {
+gulp.task('default', ['transpile'], function () {
 
     // Start browser process
     electron.start();
@@ -23,7 +21,13 @@ gulp.task('serve', ['transpile'], function () {
     gulp.watch('main.js', electron.restart);
 
     // Reload renderer process
-    gulp.watch(['index.css','index.js', 'index2.html'], electron.reload);
+    gulp.watch(['*.css', 'index.js', 'index.html'], function (fileVinyl) {
+        // do not reload renderer process if the main application has changed
+        // TODO Refactor this to separate browser and renderer source
+        if (fileVinyl.baseName === 'main.js') return;
+   
+         electron.reload()
+    });
 
     gulp.src('./*.jade')
         .pipe(jade({
